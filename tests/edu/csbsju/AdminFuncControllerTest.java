d919e08a2f524d3634505c04121a134fa0b552ae
cjzins <cjzins@csbsju.edu>
2017-04-02 22:20:56 -0500

    admin tests

---
+++ tests/edu/csbsju/AdminFuncControllerTest.java
@@ -0,0 +1,84 @@
+package edu.csbsju;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdminFuncControllerTest {
+
+	@Test
+	public void testAdminFuncController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdminFuncControllerAdmin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAccounts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddUniversity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayAccounts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUniversities() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayUniversities() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeactivate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testViewAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditUniversity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddEmphases() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveEmphases() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ tests/edu/csbsju/AdminTest.java
@@ -0,0 +1,44 @@
+package edu.csbsju;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdminTest {
+
+	@Test
+	public void testAdminStringStringStringStringCharChar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdminString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
