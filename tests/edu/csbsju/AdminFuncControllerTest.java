343f6c8b6b22dd7bc2e5f85e06026be13011031c
cjzins <cjzins@csbsju.edu>
2017-04-02 22:33:51 -0500

    hello

--- tests/edu/csbsju/AdminFuncControllerTest.java
+++ tests/edu/csbsju/AdminFuncControllerTest.java
@@ -2,9 +2,46 @@ package edu.csbsju;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AdminFuncControllerTest {
+
+	 /**
+	   * a Admin object instance variable. Used to store the current
+	   * admin object, and to be able to access methods in the Admin class
+	   */
+	  private Admin admin;
+	  private Admin admin2;
+	  /**
+	   * This is a DBController object used to access the Database
+	   */
+	  private DBController d;
+	  /**
+	   * This is a universityController object used to access methods 
+	   * in the university Controller
+	   */
+	  private UniversityController uc;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		admin = new Admin("cjzins");
+		 admin2 = new Admin("Casey", "Zins", "czins", "pass", 'Y', 'u');
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
 
 	@Test
 	public void testAdminFuncController() {
