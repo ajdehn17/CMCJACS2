5f0db3730800c8b98f6333d0ba74d725110f84a6
saschmidgall <saschmidgall@viewlincsci33.ad.csbsju.edu>
2017-04-03 00:08:49 -0500

    this has an error

--- tests/edu/csbsju/UserFuncControllerTest.java
+++ tests/edu/csbsju/UserFuncControllerTest.java
@@ -2,33 +2,74 @@ package edu.csbsju;
 
 import static org.junit.Assert.*;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserFuncControllerTest {
-
+	private UserFuncController ufc;
+	
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		ufc = new UserFuncController(new User("Jacob","Upton","JSU","Jsu--2019",'u','Y'));
+		
+		
+	}
+	
 	@Test
 	public void testUserFuncController() {
-		fail("Not yet implemented");
+		UserFuncController a = new UserFuncController();
+		User actual = a.displayStudentDetails();
+		assertEquals("The account object is null", null, actual);
 	}
 
 	@Test
 	public void testUserFuncControllerUser() {
-		fail("Not yet implemented");
+		UserFuncController a = new UserFuncController(new User("Jacob","Upton","JSU","Jsu--2019",'u','Y'));
+		Account exp = a.displayStudentDetails();
+		assertEquals("Password is " + exp.getPassword(),exp.getPassword(), "Jsu--2019");
+		assertEquals("UserName is " + exp.getUsername(),exp.getUsername(), "JSU");
+		assertEquals("First Name is " + exp.getFirstName(),exp.getFirstName(), "Jacob");
+		assertEquals("Last Name is " + exp.getLastName(),exp.getLastName(), "Upton");
+		assertEquals("Type is " + exp.getType(),exp.getType(),'u');
+		assertEquals("Status is " + exp.getStatus(),exp.getStatus(),'Y');
 	}
 
 	@Test
 	public void testAddUniversityToSavedSchools() {
-		fail("Not yet implemented");
+		ufc.addUniversityToSavedSchools("AUGSBURG");
+		ArrayList <String> l = (ArrayList<String>) ufc.getSavedSchools();
+		String a = l.get(0);
+		assertEquals("school added is "+a,a,"AUGSBURG");
+		ufc.removeUniversityFromSavedSchools("AUGSBURG");
+		
+		
 	}
 
 	@Test
 	public void testRemoveUniversityFromSavedSchools() {
-		fail("Not yet implemented");
+		ufc.addUniversityToSavedSchools("AUGSBURG");
+		ufc.removeUniversityFromSavedSchools("AUGSBURG");
+		ArrayList <String> l = (ArrayList<String>) ufc.getSavedSchools();
+		String a = l.get(0);
+		assertEquals("school added is "+a,a,null);
+		
 	}
 
-	@Test
+	@Test (expected=Error.class)
 	public void testSearchForSchools() {
-		fail("Not yet implemented");
+		ArrayList<University> m = (ArrayList<University>) ufc.searchForSchools("AUGSBURG", null,null,null,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,null);
+		assertEquals("list of schools is"+m.size(),m.size(),0);
+		ArrayList<String> emphases = new ArrayList<String>();
+		emphases.add("ENGINEERING");
+		ArrayList<University> l = (ArrayList<University>) ufc.searchForSchools("UNIVERSITY OF MINNESOTA","MINNESOTA","URBAN","STATE",40000,60000,40,50,400,500,500,600,13000,15000,40,60,8000,10000,80,90,55,65,3,5,3,5,3,5,emphases);
+		assertEquals("list of schools is"+m.size(),m.size(),0);
+		
 	}
 
 	@Test
@@ -38,17 +79,35 @@ public class UserFuncControllerTest {
 
 	@Test
 	public void testEditStudentProfile() {
-		fail("Not yet implemented");
+		ufc.editStudentProfile("Jacob","Bleh","Jsu--2019");
+		User u = ufc.displayStudentDetails();
+		assertEquals("new lastname is "+u.getLastName(),u.getLastName(),"Bleh");
+		ufc.editStudentProfile("Jacob","Upton","Jsu--2019");
+		ufc.editStudentProfile("Jacob","Upton","Bleh");
+		assertEquals("new lastname is "+u.getPassword(),u.getPassword(),"Bleh");
+		ufc.editStudentProfile("Jacob","Upton","Jsu--2019");
+		ufc.editStudentProfile("Bleh","Upton","Jsu--2019");
+		assertEquals("new lastname is "+u.getFirstName(),u.getFirstName(),"Bleh");
+		ufc.editStudentProfile("Jacob","Upton","Jsu--2019");	
+		
 	}
 
 	@Test
 	public void testGetSavedSchools() {
-		fail("Not yet implemented");
+		ufc.addUniversityToSavedSchools("AUGSBURG");
+		ArrayList <String> l = (ArrayList<String>) ufc.getSavedSchools();
+		String a = l.get(0);
+		assertEquals("school added is "+a,a,"AUGSBURG");
+		ufc.removeUniversityFromSavedSchools("AUGSBURG");
+		
 	}
 
 	@Test
 	public void testGetFiveMatches() {
-		fail("Not yet implemented");
+		List<String> emphases = Arrays.asList("ENGINEERING");
+		University u = new University("UNIVERSITY OF MINNESOTA","MINNESOTA","URBAN","STATE",40000,60000,40,50,400,500,500,600,13000,15000,40,60,8000,10000,80,90,55,65,3,5,3,5,3,5,emphases);
+		ArrayList<University> l = (ArrayList<University>) ufc.getFiveMatches(u);
+		assertEquals("list length is "+l.size(),l.size(),5);
 	}
 
 }
