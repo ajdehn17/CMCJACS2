82feadd78499940b1bd44f9ef8562efeb01560b1
cjzins <cjzins@csbsju.edu>
2017-04-02 23:03:53 -0500

    finished admin tests

--- tests/edu/csbsju/AdminTest.java
+++ tests/edu/csbsju/AdminTest.java
@@ -4,22 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AdminTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
+	private Admin admin;
+	  private Admin admin2;
 	@Before
-	public void setUp() throws Exception {
+	public void setUpBeforeClass() throws Exception {
+		admin = new Admin("cjzins");
+	   admin2 = new Admin("Casey", "Zins", "czins", "pass", 'Y', 'u');
 	}
 
 	@After
@@ -28,37 +24,60 @@ public class AdminTest {
 
 	@Test
 	public void testAdminStringStringStringStringCharChar() {
-		fail("Not yet implemented");
+		Assert.assertTrue("name is", admin2.getFirstName() == "Casey");
+	     Assert.assertTrue("last name is", admin2.getLastName() == "Zins");
+	     Assert.assertTrue("username is", admin2.getUsername() == "czins");
+	     Assert.assertTrue("password is", admin2.getPassword().equals("pass"));
+	     Assert.assertTrue("Type is", admin2.getType() == 'Y');
+	     Assert.assertTrue("status is", admin2.getStatus() == 'u');
 	}
 
 	@Test
 	public void testAdminString() {
-		fail("Not yet implemented");
+		 Assert.assertTrue("name is null", admin.getFirstName() == null);
+	     Assert.assertTrue("last name is null", admin.getLastName() == null);
+	     Assert.assertTrue("username is null", admin.getUsername() == "cjzins");
+	     Assert.assertTrue("password is null", admin.getPassword() == null);
+	     Assert.assertTrue("Type is null", admin.getType() == '\0');
+	     Assert.assertTrue("status is null", admin.getStatus() == '\0');
 	}
 
 	@Test
 	public void testSetStatus() {
-		fail("Not yet implemented");
+		 admin2.setStatus('N');
+		 Assert.assertTrue("status is N", admin2.isActive() == false);
+		 admin.setStatus('Y');
+		 Assert.assertTrue("status is Y", admin.isActive()==true);
 	}
 
 	@Test
 	public void testSetType() {
-		fail("Not yet implemented");
+		admin.setType('u');
+	    Assert.assertTrue("type is u", admin.returnType() == 'u');
+	    admin2.setType('a');
+	    Assert.assertTrue("type is a", admin2.returnType() == 'a');
 	}
 
 	@Test
 	public void testEditAccount() {
-		fail("Not yet implemented");
+		admin.editAccount("Cas", "zi", "czins", "pa", 'N', 'a');
+		Assert.assertTrue("name is", admin.getFirstName()=="Cas");
+		Assert.assertTrue("last name is", admin.getLastName()=="zi");
+		Assert.assertTrue("password is", admin.getPassword()=="pa");
+		Assert.assertTrue("status is", admin.getStatus()=='N');
+		Assert.assertTrue("status is", admin.getType()=='a');
 	}
 
 	@Test
 	public void testAddAccount() {
-		fail("Not yet implemented");
+		Account a = admin.addAccount("lori", "zins", "lvzins", "pass", 'Y', 'a');
+		Assert.assertTrue("name is", a.getFirstName()=="lori");
+		Assert.assertTrue("last name is", a.getLastName()=="zins");
+		Assert.assertTrue("password is", a.getPassword()=="pass");
+		Assert.assertTrue("status is", a.getStatus()=='Y');
+		Assert.assertTrue("status is", a.getType()=='a');
+		
 	}
 
-	@Test
-	public void testMain() {
-		fail("Not yet implemented");
-	}
 
 }
