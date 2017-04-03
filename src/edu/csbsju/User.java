101a0fb0e42d59e7dc1924a72172ead0e609017f
saschmidgall <saschmidgall@viewlincsci33.ad.csbsju.edu>
2017-04-02 22:46:52 -0500

    pushing

--- src/edu/csbsju/User.java
+++ src/edu/csbsju/User.java
@@ -47,18 +47,20 @@ public class User extends Account {
 	 * displays error if displays fails. If there is an error in getting the saved
 	 * Schools of a User, then this error is displayed
 	 */
-	public void displaySavedSchoolsError()
+	public String displaySavedSchoolsError()
 	{
 		System.out.println("There are no saved Schools in your Saved Schools List.");
+		return "There are no saved Schools in your Saved Schools List.";
 	}
 	
 	/**
 	 * displays search error. If there is an error in searching, then this error
 	 * is displayed to the user.
 	 */
-	public void displaySearchError()
+	public String displaySearchError()
 	{
 		System.out.println("No schools were found. Please try again.");
+		return "No schools were found. Please try again.";
 		
 	}
 	
