7cc6df6d60036f91d0cf0fe58a362816e53b12dc
jsupton <jsupton@viewlincsci08.ad.csbsju.edu>
2017-04-05 09:06:14 -0500

    added UseCaseTests class

---
+++ tests/edu/csbsju/UseCaseTests.java
@@ -0,0 +1,188 @@
+/**
+ * 
+ */
+package edu.csbsju;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author jsupton
+ *
+ */
+public class UseCaseTests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void UseCase1LoginSuccessful() {
+		
+	}
+	
+	@Test
+	public void UseCase1LoginInvalidUsername() {
+		
+	}
+	
+	@Test
+	public void UseCase1LoginInvalidPassword() {
+		
+	}
+	
+	@Test
+	public void UseCase1LoginDeactivated() {
+		
+	}
+	
+	@Test
+	public void UseCase2Search() {
+		
+	}
+	
+	@Test
+	public void UseCase2SearchBrowseAllSchools() {
+		
+	}
+	
+	@Test
+	public void UseCase2SearchNoSchoolsFound() {
+		
+	}
+	
+	@Test
+	public void UseCase3ViewStudentProfile() {
+		
+	}
+	
+	@Test
+	public void UseCase4ManageSavedSchools() {
+		
+	}
+	
+	@Test
+	public void UseCase4ManageSavedSchoolsNone() {
+		
+	}
+	
+	@Test
+	public void UseCase5ManageUniversities() {
+		
+	}
+	
+	@Test
+	public void UseCase6ManageUsers() {
+		
+	}
+	
+	@Test
+	public void UseCase7ViewSearchedUniversity() {
+		
+	}
+	
+	@Test
+	public void UseCase8SaveToSavedSchoolsSuccessful() {
+		
+	}
+	
+	@Test
+	public void UseCase8SaveToSavedSchoolsDuplicateSchoo() {
+		
+	}
+	
+	@Test
+	public void UseCase9SaveEditedUser() {
+		
+	}
+	
+	@Test
+	public void UseCase10RemoveSavedUniversity() {
+		
+	}
+	
+	@Test
+	public void UseCase11ViewUniversityDetails() {
+		
+	}
+	
+	@Test
+	public void UseCase12EditUniversity() {
+		
+	}
+	
+	@Test
+	public void UseCase13AddUniversity() {
+		
+	}
+	
+	@Test
+	public void UseCase13AddUniversityDuplicateSchool() {
+		
+	}
+	
+	@Test
+	public void UseCase14AddUser() {
+		
+	}
+	
+	@Test
+	public void UseCase14AddUserDuplicateUsername() {
+		
+	}
+	
+	@Test
+	public void UseCase15EditUser() {
+		
+	}
+	
+	@Test
+	public void UseCase15EditUserInvalidUsername() {
+		
+	}
+	
+	@Test
+	public void UseCase16Deactivate() {
+		
+	}
+	
+	@Test
+	public void UseCase16DeactivateDeactivatedAccount() {
+		
+	}
+	
+	@Test
+	public void UseCase17Logout() {
+		
+	}
+
+}
